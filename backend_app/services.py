85c9ac649e80fb5916931915e364b23479ec6308
wojtekczajka <w.czajka@gmail.com>
2023-04-03 00:05:45 +0200

    update form generating plots

--- backend_app/services.py
+++ backend_app/services.py
@@ -6,6 +6,7 @@ from backend_app import models, crud
 
 from backend_app.item import Item
 
+
 def find_items(db: Session):
     items = []
     db_items = crud.get_items(db)
@@ -13,14 +14,17 @@ def find_items(db: Session):
         items.append(Item(db_item.name))
     return items
 
+
 def generate_plots(db: Session):
-    products = db.query(models.Item.name).distinct().all()
+    products = crud.get_items(db)
     for product in products:
-        temp_items = db.query(models.Item).filter(models.Item.name == product[0]).all()
-        temp_df = pd.DataFrame([(item.date, item.price) for item in temp_items], columns=['date', 'price'])
-        plot = px.line(data_frame=temp_df, x='date', y='price', template='plotly_dark')
+        prices = product.prices
+        temp_df = pd.DataFrame([(price.date, price.price)
+                               for price in prices], columns=['date', 'price'])
+        plot = px.line(data_frame=temp_df, x='date',
+                       y='price', template='plotly_dark')
         plot.update_layout(margin=dict(l=0, r=0, t=0, b=0, pad=20))
         plot.update_xaxes()
         plot.update_yaxes(showgrid=False)
-        file_path = 'static/plots/' + product[0] + '.html'
+        file_path = 'backend_app/static/plots/' + product.name + '.html'
         plot.write_html(file_path, full_html=False, include_plotlyjs='cdn')
