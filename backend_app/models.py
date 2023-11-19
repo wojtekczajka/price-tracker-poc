ce2cb5f43e6b64ccbe5668f85254571a1b4a88c8
wojtek <wojtek@wojtek.wojtek.virtualbox.org>
2023-11-20 00:34:32 +0100

    update new column with item img url added.

--- backend_app/models.py
+++ backend_app/models.py
@@ -22,7 +22,7 @@ class Item(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-
+    item_img_url = Column(String)
     prices = relationship("Price", back_populates="item")
 
 
